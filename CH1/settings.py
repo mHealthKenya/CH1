a525425cbacab21897d1bb07b8602c1f88956aeb
Joel Wekesa <joelwekesa.jw@gmail.com>
2021-06-04 10:06:35 +0300

    To postgress

--- CH1/settings.py
+++ CH1/settings.py
@@ -100,27 +100,27 @@ WSGI_APPLICATION = 'CH1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('DB_NAME','CH1'),
-#         'USER': os.environ.get('DB_USER','nishauri'),
-#         'PASSWORD': os.environ.get('DB_PASS','nishauri'),
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CH1',
-        'USER': 'root',
-        'PASSWORD': '@Tit1994',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME','CH1'),
+        'USER': os.environ.get('DB_USER','nishauri'),
+        'PASSWORD': os.environ.get('DB_PASS','nishauri'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'CH1',
+#         'USER': 'root',
+#         'PASSWORD': '@Tit1994',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 CSRF_COOKIE_NAME = "XSRF-TOKEN"
 
